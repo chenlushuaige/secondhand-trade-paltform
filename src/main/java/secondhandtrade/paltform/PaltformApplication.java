c79df41ced6bdcd929e764c47ca80477af9c5d8a
xiaomingtebieshuai <123456789aaaa>
2018-05-01 17:51:18 +0800

    Initial commit

---
+++ src/main/java/secondhandtrade/paltform/PaltformApplication.java
@@ -0,0 +1,12 @@
+package secondhandtrade.paltform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaltformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaltformApplication.class, args);
+	}
+}
---
+++ src/test/java/secondhandtrade/paltform/PaltformApplicationTests.java
@@ -0,0 +1,16 @@
+package secondhandtrade.paltform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaltformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
