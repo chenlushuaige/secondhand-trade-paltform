d0e8cec57a64fa376955cdd400618975d22f7938
xiaomingtebieshuai <123456789aaaa>
2018-05-17 21:52:27 +0800

    generator产生model,mapper,xml文件

--- src/main/java/secondhandtrade/paltform/PaltformApplication.java
+++ src/main/java/secondhandtrade/paltform/PaltformApplication.java
@@ -2,8 +2,10 @@ package secondhandtrade.paltform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("secondhandtrade.paltform.*")
 public class PaltformApplication {
 
 	public static void main(String[] args) {
